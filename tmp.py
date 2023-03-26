ee3ee84146d192a6cdf8c1592049efba6d4af207
Thrawn1 <b.eremin@gmail.com>
2023-03-26 15:18:07 +0300

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,4 +1,15 @@
-tmp = 'X317.966 Z-1.93 I61.546 K-718.816'
-m = 'I'
-if m in tmp:
-    print('yes')
+name_file_1 = 'test_out_5.nc'
+name_file_2 = 'ZF_1_dug_lic_part_3_t.nc'
+
+file_1 = open(name_file_1, 'r')
+file_2 = open(name_file_2, 'r')
+count = 1
+for line_1 in file_1:
+    line_2 = file_2.readline()
+    if line_1 != line_2:
+        print('Строка', count)
+        print(line_1)
+        print(line_2)
+        print('-----------------')
+        break
+    count += 1
