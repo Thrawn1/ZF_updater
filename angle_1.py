e0c2afb277f04bcf0580d8d78a1c1cb4bff8eaac
Thrawn1 <b.eremin@gmail.com>
2023-04-06 22:30:38 +0300

    Update angle_1.py

--- angle_1.py
+++ angle_1.py
@@ -1,72 +1,74 @@
 from math import sqrt, acos,pi
 
-#Модуль надо переименовать
-#Разбить функцию tmp_name на несколько функций. В частности - функцию вычисления радиуса дуги
-#Функцию вычисления угла малой дуги окончания движения(что бы можно было передать в нее значения длины дуги окончания)
-#Функцию-парсер строк с командами 
-#Функцию-справку о дуге - радиус, длина, угол, начало, конец и т.д.
-
-def angle(a, b):
-    a_start_x, a_start_y, a_end_x, a_end_y = a
-    b_start_x, b_start_y, b_end_x, b_end_y = b
-    a_length = sqrt((a_end_x - a_start_x)**2 + (a_end_y - a_start_y)**2)
-    b_length = sqrt((b_end_x - b_start_x)**2 + (b_end_y - b_start_y)**2)
-    if round(a_length, 4) == round(b_length, 4):
-        R = a_length
+def calculate_length_line(start:tuple, end:tuple, flag = 0) -> float:
+    """Функция вычисляет длину отрезка по координатам его концов."""
+    
+    x1, y1 = start
+    x2, y2 = end
+    length = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    if flag != 0:
+        return length
     else:
-        print('Ошибка определения радиуса!')
-        return 0,0
-    c_length = sqrt((a_end_x - b_end_x)**2 + (a_end_y - b_end_y)**2)
-    angle_rad = acos((a_length**2 + b_length**2 - c_length**2) / (2 * a_length * b_length))
-    angle_grad = angle_rad * 180 / pi
-    return angle_grad,R
+        return round(length, 4)   
 
+def calculate_angle_between_line(line_1:tuple, line_2:tuple) -> float:
+    """Функция вычисляет угол между двумя отрезками по координатам их концов."""
+    
+    start_line_1,end_line_1 = line_1
+    start_line_2, end_line_2 = line_2
+    start_line_3 = end_line_1
+    end_line_3 = end_line_2
+    length_line_1 = calculate_length_line(start_line_1, end_line_1,1)
+    length_line_2 = calculate_length_line(start_line_2, end_line_2,1)
+    length_line_3 = calculate_length_line(start_line_3, end_line_3,1)
+    angle_between_line_rad   = acos((length_line_1 + length_line_2 - length_line_3**2) / (2 * length_line_1 * length_line_2))
+    return angle_between_line_rad
 
-def tmp_name(text):
-    data_dict = {}
-    vc = []
+def parse_line_command(line:str,data_information: dict) -> tuple:
+    """Функция парсит строку с командой и возвращает словарь с данными и список с векторами смещения."""
+
+    if line.startswith('G2'):
+        line_list = line.split()
+        for i in line_list:
+            if i.startswith('X'):
+                x = float(i[1:])
+                data_information['X2_end'] = x
+            elif i.startswith('Z'):
+                z = float(i[1:])
+                data_information['Z2_end'] = z
+            elif i.startswith('I'):
+                i = float(i[1:])
+                data_information['I'] = i
+            elif i.startswith('K'):
+                k = float(i[1:])
+                data_information['K'] = k
+    else:
+        line_list = line.split()
+        for i in line_list:
+            if i.startswith('X'):
+                x = float(i[1:])
+                data_information['X1_end'] = x
+            elif i.startswith('Z'):
+                z = float(i[1:])
+                data_information['Z1_end'] = z
+    return data_information
+
+def arc_end_safety_angle(text:str, length_arc_safety:float) -> float:
+    data_information = {}
     for line in text:
-        if line.startswith('G2'):
-            line_list = line.split()
-            for i in line_list:
-                if i.startswith('X'):
-                    x = float(i[1:])
-                    data_dict['X2_end'] = x
-                elif i.startswith('Z'):
-                    z = float(i[1:])
-                    data_dict['Z2_end'] = z
-                elif i.startswith('I'):
-                    i = float(i[1:])
-                    vc.append(i)
-                elif i.startswith('K'):
-                    k = float(i[1:])
-                    vc.append(k)
-        else:
-            line_list = line.split()
-            for i in line_list:
-                if i.startswith('X'):
-                    x = float(i[1:])
-                    data_dict['X1_end'] = x
-                elif i.startswith('Z'):
-                    z = float(i[1:])
-                    data_dict['Z1_end'] = z
-    X_start = data_dict['X1_end'] + vc[0]
-    Z_start = data_dict['Z1_end'] + vc[1]
-    print(X_start, Z_start)
-    data_dict['X_start'] = X_start
-    data_dict['Z_start'] = Z_start
-    a_start = (data_dict['X_start'], data_dict['Z_start'], data_dict['X1_end'], data_dict['Z1_end'])
-    b_end  = (data_dict['X_start'], data_dict['Z_start'], data_dict['X2_end'], data_dict['Z2_end'])
-    angl,R = angle(a_start, b_end)
-    l_dug = 2 * pi * R / 360 * angl
-    print('Радиус дуги: ', R) 
-    print('lenght arc: ', l_dug)
-    l_dug_secured = 12
-    new_angl = l_dug_secured*360/(2 * pi * R)
-    new_angl_rad = new_angl * pi / 180
-    return new_angl_rad
-
-
-text = ['X495.8709 Z-41.2622','G2 X16.0  Z-41.2622 I-239.9355 K-676.9691']
-angl = tmp_name(text)
-print(angl)
+        parse_line_command(line,data_information)
+    X_start = data_information['X1_end'] + data_information['I']
+    Z_start = data_information['Z1_end'] + data_information['K']
+    data_information['X_start'] = X_start
+    data_information['Z_start'] = Z_start
+    a_start = (data_information['X_start'], data_information['Z_start'], data_information['X1_end'], data_information['Z1_end'])
+    b_end  = (data_information['X_start'], data_information['Z_start'], data_information['X2_end'], data_information['Z2_end'])
+    arc_full_angl,R = calculate_angle_between_line(a_start, b_end)
+    length_full_arc = 2 * pi * R / 360 * arc_full_angl
+    data_information['length_full_arc'] = length_full_arc
+    data_information['length_arc_safety'] = length_arc_safety
+    safety_angle_arc_deg = length_arc_safety*360/(2 * pi * R)
+    safety_angle_arc_rad = safety_angle_arc_deg * pi / 180
+    data_information['safety_angle_arc_deg'] = safety_angle_arc_deg
+    data_information['safety_angle_arc_rad'] = safety_angle_arc_rad
+    return data_information
