8a98c200df6d30a1a012e2dcc0d71c0c2ffb8b09
Thrawn1 <b.eremin@gmail.com>
2023-04-07 11:04:32 +0300

    Update trigangle.py

--- trigangle.py
+++ trigangle.py
@@ -2,47 +2,48 @@ from math import cos, sin, atan2, sqrt, pi
 from matplotlib import pyplot as plt
 from work_arc import calculate_length_line, calculate_angle_between_line, parse_line_command, arc_end_safety_angle
 
-#Входные данные 
-
-
-Xa = 16.0
-Ya = -41.2622
-Xb = 255.9354
-Yb = -718.2313
-r = 718.2313
-angle_rad = 0.016707709619449892 # противолежащий основанию угол в радианах
-
 
 def calculate_coordinates_end_safety_arc(data_information:dict) -> tuple:
     """Функция вычисляет координаты конца безопасной дуги."""
-
-    delta_x_axe_1 = round((data_information['X2_end'] - data_information['X_centr_arc']),4)
-    delta_z_axe_1 = round((data_information['Z2_end'] - data_information['Z_centr_arc']),4)
-    delta_x_axe_2 = round((delta_x_axe_1*cos(angle_rad) + delta_z_axe_1*sin(data_information['safety_angle_arc_rad'])),4)
-    delta_z_axe_2 = round((-delta_x_axe_1*sin(angle_rad) + delta_z_axe_1*cos(angle_rad)),4)
-    xsa = round((data_information['X_centr_arc'] + delta_x_axe_2),4)
-    zsa = round((data_information['Z_centr_arc'] + delta_z_axe_2),4)
-    print( 'X_safety_arc_end = ', xsa, 'Z_safety_arc_end = ', zsa)
-    return (xsa, zsa)
+    angle_safety_arc = data_information['safety_angle_arc_rad']
+    delta_x_axe_1 = data_information['X2_end'] - data_information['X_centr_arc']
+    delta_z_axe_1 = data_information['Z2_end'] - data_information['Z_centr_arc']
+    delta_x_axe_2 = delta_x_axe_1*cos(angle_safety_arc) + delta_z_axe_1*sin(angle_safety_arc)
+    delta_z_axe_2 = -delta_x_axe_1*sin(angle_safety_arc) + delta_z_axe_1*cos(angle_safety_arc)
+    xsa = data_information['X_centr_arc'] + delta_x_axe_2
+    zsa = data_information['Z_centr_arc'] + delta_z_axe_2
+    data_information['X_safety_arc_end'] = xsa
+    data_information['Z_safety_arc_end'] = zsa
+    return (xsa, zsa),data_information
 
 
 def calculate_vector_center_arc(data_information:dict) -> tuple:
-    pass
+    print(data_information)
+    I_safety_arc_raw = data_information['X_centr_arc'] - data_information['X_safety_arc_end']
+    K_sefety_arc_raw = data_information['Z_centr_arc'] - data_information['Z_safety_arc_end']
+    I_safety_arc = round(I_safety_arc_raw,4)
+    K_sefety_arc = round(K_sefety_arc_raw,4)
+    print(I_safety_arc,K_sefety_arc)
+    return (I_safety_arc,K_sefety_arc)
 
-
+def build_text_command(data_information:dict,vec:tuple) -> list:
+    list_text = []
+    line_1 = 'G1 ' + 'X' + str(data_information['X1_end']) + ' ' + 'Z' + str(data_information['Z1_end']) + ' F30'
+    print(line_1)
+    list_text.append(line_1)
+    line_2 = 'G' + str(data_information['G']) + ' ' + 'X' + str(round((data_information['X_safety_arc_end']),4)) + ' ' + 'Z' + str(round((data_information['Z_safety_arc_end']),4)) + ' ' + 'I' + str(data_information['I']) + ' ' + 'K' + str(data_information['K']) + ' F3000'
+    print(line_2)
+    list_text.append(line_2)
+    line_3 = 'X' + str(round((data_information['X2_end']),4)) + ' ' + 'Z' + str(round((data_information['Z2_end']),4)) + ' ' + 'I' + str(vec[0]) + ' ' + 'K' + str(vec[1]) + ' F30'
+    print(line_3)
+    list_text.append(line_3)
 text = ['X495.8709 Z-41.2622','G2 X16.0  Z-41.2622 I-239.9355 K-676.9691']
 t = arc_end_safety_angle(text, 12)
 print(t)
-o = calculate_coordinates_end_safety_arc(t)
+o,yy = calculate_coordinates_end_safety_arc(t)
 print(o)
-    # x3 = Xc
-    # y3 = Yc
-    # I_new = Xb-x3
-    # K_new = Yb-y3
-    # I_new = round(I_new, 4)
-    # K_new = round(K_new, 4)
-    # print(I_new, K_new)
-
+tt = calculate_vector_center_arc(yy)
+build_text_command(yy,tt)
 
 #Визуализация
 # plt.axes().set_aspect('equal')
